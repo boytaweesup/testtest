7c48fec09861c0056914c3a402d50f7b4888d550
boytaweesup <85148639+boytaweesup@users.noreply.github.com>
2024-11-06 00:37:38 +0700

    Update loginregister.js

--- logininputphpoj/loginregister.js
+++ logininputphpoj/loginregister.js
@@ -1,10 +1,12 @@
 const wrapper = document.querySelector(".wrapper");
 const loginLink = document.querySelector(".login-link");
 const registerLink = document.querySelector(".register-link");
-const btnPopup = document.getElementById("btnLogin-popup");
+// const btnPopup = document.getElementById("btnLogin-popup");
 const iconClose = document.querySelector(".icon-close");
 const addFormMember = document.getElementById("add-member-form");
 const loginFormMember = document.getElementById("login-form");
+const addFormProduct = document.getElementById("add-product-form");
+const addFormOrder = document.getElementById("add-order-form");
 
 registerLink.addEventListener("click", () => {
   wrapper.classList.add("active");
@@ -13,10 +15,14 @@ registerLink.addEventListener("click", () => {
 loginLink.addEventListener("click", () => {
   wrapper.classList.remove("active");
 });
-
-btnPopup.addEventListener("click", () => {
+function btnLogin_popup(e) {
+  e.preventDefault();
   wrapper.classList.add("active-popup");
-});
+}
+
+// btnPopup.addEventListener("click", () => {
+//   wrapper.classList.add("active-popup");
+// });
 
 iconClose.addEventListener("click", () => {
   wrapper.classList.remove("active-popup");
@@ -24,25 +30,52 @@ iconClose.addEventListener("click", () => {
 
 addFormMember.addEventListener("submit", async (e) => {
   e.preventDefault();
-
+console.log(addFormMember);
   const formData = new FormData(addFormMember);
   formData.append("addmember", 1);
-      
-    const data = await fetch("action.php", {
-      method: "POST",
-      body: formData,
-    }).then(function (res) {
-      
-      Swal.fire(
-        "Registration completed!",
-        "You clicked the button!",
-        "success"
-      );
-      setTimeout(function () {
-        window.location.reload()
-      }, 2000);
-    });
-  
+
+  const data = await fetch("action.php", {
+    method: "POST",
+    body: formData,
+  }).then(function (res) {
+    Swal.fire("Registration completed!", "You clicked the button!", "success");
+    setTimeout(function () {
+      window.location.reload();
+    }, 2000);
+  });
+});
+addFormProduct.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(addFormProduct);
+  formData.append("addproduct", 1);
+
+  const data = await fetch("action.php", {
+    method: "POST",
+    body: formData,
+  }).then(function (res) {
+    Swal.fire("Add Product completed!", "You clicked the button!", "success");
+    setTimeout(function () {
+      window.location.reload();
+    }, 2000);
+  });
+});
+
+addFormOrder.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(addFormOrder);
+  formData.append("addorder", 1);
+
+  const data = await fetch("action.php", {
+    method: "POST",
+    body: formData,
+  }).then(function (res) {
+    Swal.fire("Add Order completed!", "You clicked the button!", "success");
+    setTimeout(function () {
+      window.location.reload();
+    }, 2000);
+  });
 });
 
 loginFormMember.addEventListener("submit", async (e) => {
@@ -55,21 +88,14 @@ loginFormMember.addEventListener("submit", async (e) => {
     method: "POST",
     body: formData,
   }).then(function (res) {
-    
-    Swal.fire(
-      "Registration completed!",
-      "You clicked the button!",
-      "success"
-    );
+    Swal.fire("login completed!", "You clicked the button!", "success");
     setTimeout(function () {
-      window.location.reload()
+      window.location.reload();
     }, 2000);
   });
-
 });
 
 function logout(e) {
- 
   e.preventDefault();
 
   Swal.fire({
@@ -79,11 +105,33 @@ function logout(e) {
     showCancelButton: true,
     confirmButtonColor: "#3085d6",
     cancelButtonColor: "#d33",
-    confirmButtonText: "Yes, logout"
+    confirmButtonText: "Yes, logout",
   }).then((result) => {
     if (result.isConfirmed) {
-      window.location.href = 'logout.php';
+      window.location.href = "logout.php";
     }
   });
-  
 }
+
+const fetchorder = async () => {
+  const formData = new FormData();
+  formData.append("selectorder", 1);
+
+  const data = await fetch("action.php", {
+    method: "POST",
+    body: formData,
+  });
+  const response = await data.json();
+  response.forEach((element) => {
+    // console.log(element.Product_Name);
+    document.getElementById("select_order").innerHTML +=
+      '<option value="' +
+      element.Product_Name +
+      '">' +
+      element.Product_Name +
+      "</option>";
+  });
+
+  // tbody.innerHTML = response;
+};
+fetchorder();
